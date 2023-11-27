a0c1dabfde98e6fed7aeb56999ed4399ac9d87ed
Ian-Collins1252 <ian-collins@ku.edu>
2023-11-26 22:56:40 -0600

    Added functions for repeated and hanging operators - Ian Collins

--- errors.cpp
+++ errors.cpp
@@ -4,15 +4,56 @@
 //Course: EECS 348
 //Purpose: Handle the errors that can occur within the caluclator
 
-#include <string.h>
-using std::string;
+#include <string>
 
-int invalid_char(string expression):
-    for (int i = 0, i < expression.length(), i++):
-        if !('0' <= expression[i] <= '9' || expression[i] == ('+'||'-'||'*'||'/'||'('||')'||'^'||'%')):
+//Check if a given char is a num
+bool is_num(char num) {
+    if ((num >= '0')&&(num <= '9')) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//Check if a given char is an operator
+bool is_operator(char sym) {
+    if ((sym == '%')||(sym == '*')||(sym == '+')||(sym == '-')||(sym == '^')||(sym == '/')) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//Checks if there are any invalid chars in the string
+int invalid_char(string expr) {
+    for (int i = 0; i < expr.length(); i++) {
+        char c = expr[i];
+        //Checks range using ascii values
+        if (!(is_num(c)||is_operator(c))) {
             return 1;
-            
+        }
+    }
     return 0;
+}
 
-int invalid_paran(string expression):
+//Checks if there are any invalid operators
+int invalid_operator(string expr) {
+    //Find last index of given string
+    int n = expr.length()-1;
+
+    //Looks if first or last char in the string is an operator
+    if (is_operator(expr[0])||is_operator(expr[n])) {
+        return 1;
+    } else {
+        return 0;
+    }
+
+    //Looks for repeated operator
+    for (int i=0; i < expr.length()-1; i++) {
+        if (is_operator(expr[i]) && is_operator(expr[i])) {
+            return 1;
+        }
+    }
     return 0;
+}
+
